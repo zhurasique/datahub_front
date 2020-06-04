9ba197e4e2488492dbc5c3f8fbdcff7a45a54fa9
zhurasique <berezovsky.serega@gmail.com>
2020-06-04 19:32:19 +0200

    js changes, vanilla

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,15 @@
 let departmentApi = "http://localhost:8080/api/department";
-let typeApi = "/api/type";
+let typeApi = "http://localhost:8080/api/type/";
 let productApi = "/api/product";
 
-var vue = new Vue({
+let chosenDepartmentId = 3;
+
+var departments = new Vue({
     el: "#departments",
     data: function(){
         return {
-            departments: []
+            departments: [],
+            types: []
         }
     },
 
@@ -23,9 +26,51 @@ var vue = new Vue({
             });
 
         },
+
+        loadTypes: function () {
+            this.types = [];
+            axios({
+                method: "get",
+                url: typeApi + "filter/department/id?id=" + chosenDepartmentId
+            })
+                .then(response => {
+                    this.types = response.data;
+                }).catch(error => {
+                console.log(error);
+            });
+        }
     },
     created: function () {
         this.loadDepartments(this.departments);
+        this.loadTypes(this.types);
+    }
+});
+
+var types = new Vue({
+    el: "#types",
+    data: function(){
+        return {
+            types: []
+        }
+    },
+
+    methods: {
+        loadTypes: function () {
+            this.types = [];
+            axios({
+                method: "get",
+                url: typeApi + "filter/department/id?id=" + chosenDepartmentId
+            })
+                .then(response => {
+                    this.types = response.data;
+                    console.log(response.data);
+                }).catch(error => {
+                console.log(error);
+            });
+        }
+    },
+    created: function () {
+        this.loadTypes(this.types);
     }
 });
 
@@ -37,11 +82,27 @@ document.addEventListener('DOMContentLoaded', function(){
 
         for (let i = 0; i < departments.length; i++) {
             departments[i].addEventListener('mouseover', function()  {
+                chosenDepartmentId = departments[i];
                 types.style.display = "unset";
             }, false);
             departments[i].addEventListener('mouseout',  function() {
-                types.style.display = "none";
+                typesCheckActive("type-bar");
             }, false);
         }
+
+        function typesCheckActive(e) {
+            let count = 0;
+            let types = document.getElementById(e);
+
+            types.addEventListener('mouseover', function()  {
+                count++;
+            }, false);
+            types.addEventListener('mouseout',  function() {
+                if(count > 0)
+                    types.style.display = "none";
+            }, false);
+        }
+
+
     }, 100);
 });

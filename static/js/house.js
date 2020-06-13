404a6364ebb6f7e7f49a73111d99a6c44eeb0847
zhurasique <berezovsky.serega@gmail.com>
2020-06-13 13:47:07 +0200

    added house page

---
+++ static/js/house.js
@@ -0,0 +1,50 @@
+let departmentName = "Dom i ogrod";
+let typeApi = "http://localhost:8080/api/type/";
+let categoryApi = "http://localhost:8080/api/category/";
+
+var types = new Vue({
+    el: "#types",
+    data: function(){
+        return {
+            types: [],
+            categories: [],
+            images: [],
+            departmentName: departmentName
+        }
+    },
+
+    methods: {
+        loadTypes: function () {
+            this.types = [];
+            axios({
+                method: "get",
+                url: typeApi + "department/name?department=" + departmentName
+            })
+                .then(response => {
+                    this.types = response.data;
+                    for(let i = 0; i < this.types.length; i++) {
+                        this.images.push("./static/img/" + this.types[i].name.toLocaleLowerCase().trim().replace(/ /g,"-") + ".jpg");
+                        //this.links.push("/dzial/" + this.types[i].name.toLocaleLowerCase().trim().replace(/ /g,"-"))
+                    }
+                }).catch(error => {
+                console.log(error);
+            });
+        },
+        loadCategories: function () {
+            this.categories = [];
+            axios({
+                method: "get",
+                url: categoryApi
+            })
+                .then(response => {
+                    this.categories = response.data;
+                }).catch(error => {
+                console.log(error);
+            });
+        }
+    },
+    created: function () {
+        this.loadTypes(this.types);
+        this.loadCategories(this.categories);
+    }
+});
